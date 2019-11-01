782181e7935bb8cb0aa076f915d2899442f6a562
Rucha Patki <ruchapatki@gmail.com>
2019-11-01 19:54:23 +0000

    Added new view for the music

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
     def show
         render template: "pages/about"
     end
+    
+    def to_music
+        render template: "pages/music"
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root :controller => 'pages', :action => 'show', :page => 'about'
   devise_for :users
+  
+  get 'pages/music' => 'pages#music'
 end
