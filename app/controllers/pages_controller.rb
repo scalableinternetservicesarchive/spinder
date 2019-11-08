db4182de4c793f4c5bbc7c8d868a20837d496c29
Matthew Wang <matthew.wang.c@gmail.com>
2019-11-07 17:36:24 -0800

    Merged code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,6 +9,7 @@ class PagesController < ApplicationController
     def profile
         if current_user
             @email = current_user.email
+            #@songs = ActiveRecord::Base.connection.execute("SELECT l.songs FROM songs l JOIN song_infos r ON songs.song_id = song_infos")
         else
         end
     end
