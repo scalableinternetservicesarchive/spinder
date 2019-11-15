a5217b33a0933a53107c504a1c30267742a85aac
Matthew Wang <matthew.wang.c@gmail.com>
2019-11-14 20:51:14 -0800

    Fixed matches dislike button bug

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,29 +2,52 @@ class PagesController < ApplicationController
     def home
     end
 
+    # GET pages/music
     def music
-        @songs = ActiveRecord::Base.connection.execute("SELECT r.name, r.song_id FROM songs l JOIN song_infos r ON r.song_id = l.song_id WHERE l.user_email = '#{params["user_email"]}';")
+
+        # Get all songs from database
+        @songs = ActiveRecord::Base.connection.execute("""
+            SELECT r.name, r.song_id 
+            FROM songs l JOIN song_infos r ON r.song_id = l.song_id 
+            WHERE l.user_email = '#{params["user_email"]}';
+        """)
+        
+        # Extract songs that user has already liked
         song_liked = []
         for i in @songs
             song_liked.push(i["song_id"])
         end
-        puts "sup"
-        puts song_liked
-        @song = SongInfo.where(song_id: song_liked).order('RANDOM()').first
-        puts @song
+
+        # Only pick a random song that the user HASNT liked before
+        @song = SongInfo.where.not(song_id: song_liked).order('RANDOM()').first
     end
 
+    # GET pages/profile
     def profile
         if current_user
             @email = params["user_email"]
-            @songs = ActiveRecord::Base.connection.execute("SELECT r.name, r.song_id FROM songs l JOIN song_infos r ON r.song_id = l.song_id WHERE l.user_email = '#{params["user_email"]}';")
+
+            # Get all the songs that a user likes
+            @songs = ActiveRecord::Base.connection.execute("""
+                SELECT r.name, r.song_id 
+                FROM songs l JOIN song_infos r ON r.song_id = l.song_id 
+                WHERE l.user_email = '#{params["user_email"]}';
+            """)
+
+            # Extract only the song id's (song_ids required to find matches in next query)
             song_id_arr = []
             for i in @songs
                 song_id_arr.push(i["song_id"])
             end
-            puts song_id_arr.join(', ')
-            query = "SELECT user_email, COUNT(*) as cnt FROM songs WHERE (song_id IN ('#{song_id_arr.join("', '")}')) and user_email != '#{params["user_email"]}' GROUP BY user_email ORDER BY cnt DESC;"
-            @matches = ActiveRecord::Base.connection.execute(query)
+
+            # Run query that computes the matches that a user has, sorted from greatest to least
+            @matches = ActiveRecord::Base.connection.execute("""
+                SELECT user_email, COUNT(*) AS cnt 
+                FROM songs 
+                WHERE (song_id IN ('#{song_id_arr.join("', '")}')) AND user_email != '#{params["user_email"]}' 
+                GROUP BY user_email 
+                ORDER BY cnt DESC;
+            """)
         else
         end
     end
--- app/controllers/song_controller.rb
+++ app/controllers/song_controller.rb
@@ -2,6 +2,6 @@ class SongController < ApplicationController
     def like
         object = Song.new(:user_email => current_user.email, :song_id => params["song_id"])
         object.save
-        redirect_to pages_music_path
+        redirect_to controller: 'pages', action:'music', user_email: current_user.email
     end
 end
