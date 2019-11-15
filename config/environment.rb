b97081366d4dce25ece8471466f63742af3f3e04
Matthew Wang <matthew.wang.c@gmail.com>
2019-11-15 13:47:41 -0800

    removed seeds.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 #load the seeds for the db default values
-Rails.application.load_seed
+#Rails.application.load_seed
