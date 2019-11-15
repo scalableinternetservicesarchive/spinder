b6e135de035cf4f9d0736b8acbf565e2caa86be5
Anirudh Balasubramaniam <anirudhnkl@gmail.com>
2019-11-14 17:57:29 -0800

    files

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,13 +3,28 @@ class PagesController < ApplicationController
     end
 
     def music
-        @song = SongInfo.order('RANDOM()').first
+        @songs = ActiveRecord::Base.connection.execute("SELECT r.name, r.song_id FROM songs l JOIN song_infos r ON r.song_id = l.song_id WHERE l.user_email = '#{params["user_email"]}';")
+        song_liked = []
+        for i in @songs
+            song_liked.push(i["song_id"])
+        end
+        puts "sup"
+        puts song_liked
+        @song = SongInfo.where(song_id: song_liked).order('RANDOM()').first
+        puts @song
     end
 
     def profile
         if current_user
             @email = params["user_email"]
-            @songs = ActiveRecord::Base.connection.execute("SELECT r.name FROM songs l JOIN song_infos r ON r.song_id = l.song_id WHERE l.user_email = '#{params["user_email"]}';")
+            @songs = ActiveRecord::Base.connection.execute("SELECT r.name, r.song_id FROM songs l JOIN song_infos r ON r.song_id = l.song_id WHERE l.user_email = '#{params["user_email"]}';")
+            song_id_arr = []
+            for i in @songs
+                song_id_arr.push(i["song_id"])
+            end
+            puts song_id_arr.join(', ')
+            query = "SELECT user_email, COUNT(*) as cnt FROM songs WHERE (song_id IN ('#{song_id_arr.join("', '")}')) and user_email != '#{params["user_email"]}' GROUP BY user_email ORDER BY cnt DESC;"
+            @matches = ActiveRecord::Base.connection.execute(query)
         else
         end
     end
