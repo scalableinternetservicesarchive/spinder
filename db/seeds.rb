8571fe039f89ff08b43dbb00a2c1d30b7481d216
Matthew Wang <matthew.wang.c@gmail.com>
2019-12-03 17:32:33 -0800

    Updated seeds.rb to new version

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,18 +39,18 @@ end
 
 # Load fake users
 cur_user_domain ="@test.com"
-
-def encrypt_pass(password)
-    ::BCrypt::Password.create(password, cost: 12).to_s
-end
-
+  
 values = ""
+song_values = ""
 500.times { |n|
-    email = "a" + n.to_s + cur_user_domain
-    encrypted_password = encrypt_pass('password')
-    values.concat("('#{email}', '#{encrypted_password}', '#{Time.now}', '#{Time.now}'),")
+  email = "a" + n.to_s + cur_user_domain
+  song_id = "3SdTKo2uVsxFblQjpScoHy"
+  encrypted_password = '$2y$13$0Uz7oBtWPr.tEi8XNYTEaO2LZl64/4skBndMz1V..gBivS3nPamjO'
+  values.concat("('#{email}', '#{encrypted_password}', '#{Time.now}', '#{Time.now}'),")
+  song_values.concat("('#{email}', '#{song_id}', '#{Time.now}', '#{Time.now}'),")
 }
 
 values = values[0...-1]
+song_values = song_values[0...-1]
 ActiveRecord::Base.connection.execute("INSERT INTO users (email, encrypted_password, created_at, updated_at) VALUES #{values}")
-   
+ActiveRecord::Base.connection.execute("INSERT INTO songs (user_email, song_id, created_at, updated_at) VALUES #{song_values}")
