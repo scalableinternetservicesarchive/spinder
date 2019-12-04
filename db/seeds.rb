90c6d940ff9465cff4c8780581d27509e6c26dda
Matthew Wang <matthew.wang.c@gmail.com>
2019-12-03 16:28:07 -0800

    Moved user adding to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,10 @@
 # t.string "image_url"
 
 require "json"
+require "devise"
+require "bcrypt"
 
+# Load songs
 song_data = JSON.parse(File.read(File.dirname(__FILE__) + "/out.json"))["items"]
 
 for song in song_data do
@@ -25,4 +28,22 @@ for song in song_data do
         image_url: song["track"]["album"]["images"][0]["url"]
     })
       
-   end
+end
+
+# Load fake users
+cur_user_domain ="@test.com"
+
+def encrypt_pass(password)
+    ::BCrypt::Password.create(password, cost: 12).to_s
+end
+
+values = ""
+500.times { |n|
+    email = "a" + n.to_s + cur_user_domain
+    encrypted_password = encrypt_pass('password')
+    values.concat("('#{email}', '#{encrypted_password}', '#{Time.now}', '#{Time.now}'),")
+}
+
+values = values[0...-1]
+ActiveRecord::Base.connection.execute("INSERT INTO users (email, encrypted_password, created_at, updated_at) VALUES #{values}")
+   
